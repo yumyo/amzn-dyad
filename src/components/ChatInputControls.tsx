414ac90641f7bc12ef4de298af7d92d82c449836
Giulia Pernice <gpernice@amazon.co.uk>
2026-03-20 09:44:32 +0000

    Remove Pro mode selector from chat input controls
    - Remove ProModeSelector component from chat input area
    - This was the sparkles icon with "Pro" text next to the model picker
    - Remove unused ProModeSelector import

--- src/components/ChatInputControls.tsx
+++ src/components/ChatInputControls.tsx
@@ -1,6 +1,5 @@
 import { ContextFilesPicker } from "./ContextFilesPicker";
 import { ModelPicker } from "./ModelPicker";
-import { ProModeSelector } from "./ProModeSelector";
 import { ChatModeSelector } from "./ChatModeSelector";
 import { McpToolsPicker } from "@/components/McpToolsPicker";
 import { useSettings } from "@/hooks/useSettings";
@@ -34,7 +33,6 @@ export function ChatInputControls({
       )}
       <div className="w-1.5"></div>
       <ModelPicker />
-      <ProModeSelector />
       {showContextFilesPicker && <ContextFilesPicker />}
     </div>
   );
